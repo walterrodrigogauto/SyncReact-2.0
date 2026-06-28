519338292267a800024ea7e8818ad40c062d8d44
walterrodrigogauto <49457585+walterrodrigogauto@users.noreply.github.com>
2026-06-28 13:50:09 -0300

    Update sync.js

--- src/sync.js
+++ src/sync.js
@@ -152,8 +152,6 @@ function endReaction() {
 
   reactionBtn.textContent = '▶️ Iniciar reacción';
   reactionBtn.disabled = true;
-const videoId = playerA.getVideoData().video_id;
-saveReactionToDB(videoId, syncEvents);
   exportSyncJSON();
 
   // LIMPIEZA
